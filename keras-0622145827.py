fde267b1464c2f85222d2b4c7ba8e9f313855b12
NuLL1011 <128481802+NuLL1011@users.noreply.github.com>
2023-06-22 17:58:28 +0300

    Pushed DAG keras-0622145827

---
+++ keras-0622145827.py
@@ -0,0 +1,51 @@
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.kubernetes.volume import Volume
+from airflow.kubernetes.secret import Secret
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "keras-0622145827",
+}
+
+dag = DAG(
+    "keras-0622145827",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.15.0 pipeline editor using `keras.ipynb`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: examples/pipelines/mnist/keras.ipynb
+
+op_9a9bfeaa_29ae_4b62_936a_a931cb9b8a9c = KubernetesPodOperator(
+    name="keras",
+    namespace="default",
+    image="continuumio/anaconda3@sha256:a2816acd3acda208d92e0bf6c11eb41fda9009ea20f24e123dbf84bb4bd4c4b8",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'keras' --cos-endpoint http://172.17.0.3:9000 --cos-bucket test --cos-directory 'keras-0622145827' --cos-dependencies-archive 'keras-9a9bfeaa-29ae-4b62-936a-a931cb9b8a9c.tar.gz' --file 'examples/pipelines/mnist/keras.ipynb' "
+    ],
+    task_id="keras",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio99",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "keras-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
